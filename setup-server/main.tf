3b854ebed881e2a4d651bdee9a75bb89e31518f3
Christochi <tochukwui2@gmail.com>
2022-02-02 23:30:49 -0500

    corrected a comment in the root module

--- setup-server/main.tf
+++ setup-server/main.tf
@@ -1,7 +1,7 @@
 # compile module aws-ec2
 module "instance" {
 
-  source = "../modules/aws-ec2" # directory of the vpc module
+  source = "../modules/aws-ec2" # directory of the aws-ec2 module
   create = true # true = creates resource, false = does not create
 
 }
