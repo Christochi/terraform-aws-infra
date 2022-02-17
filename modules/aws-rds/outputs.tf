b5e7a616f929fea0c697a64c81c6e20829a6b6d1
Christochi <tochukwui2@gmail.com>
2022-02-16 20:45:15 -0500

    format output variable to be more readable

--- modules/aws-rds/outputs.tf
+++ modules/aws-rds/outputs.tf
@@ -1,6 +1,6 @@
 output subnet-ids {
 
-  value = aws_db_subnet_group.rds_subnet_group.subnet_ids
+  value = aws_db_subnet_group.rds_subnet_group.subnet_ids 
 
 }
 
@@ -8,16 +8,16 @@ output "db-data" {
 
   value = {
 
-    allocated-storage = aws_db_instance.db[*].allocated_storage
-    az                = aws_db_instance.db[*].availability_zone
-    endpoint          = aws_db_instance.db[*].endpoint
-    engine            = aws_db_instance.db[*].engine
-    engine-version    = aws_db_instance.db[*].engine_version
-    instance-class    = aws_db_instance.db[*].instance_class
-    name              = aws_db_instance.db[*].name
-    port              = aws_db_instance.db[*].port
-    status            = aws_db_instance.db[*].status
-    username          = aws_db_instance.db[*].username
+    allocated-storage = join( "", aws_db_instance.db[*].allocated_storage )
+    az                = join( "", aws_db_instance.db[*].availability_zone )
+    endpoint          = join( "", aws_db_instance.db[*].endpoint )
+    engine            = join( "", aws_db_instance.db[*].engine )
+    engine-version    = join( "", aws_db_instance.db[*].engine_version )
+    instance-class    = join( "", aws_db_instance.db[*].instance_class )
+    name              = join( "", aws_db_instance.db[*].name )
+    port              = join( "", aws_db_instance.db[*].port )
+    status            = join( "", aws_db_instance.db.*.status )
+    username          = join( "", aws_db_instance.db.*.username )
 
   }
 
