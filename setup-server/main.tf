573c6ddc5a3108520f22fd0947f0368576845c8d
Christochi <tochukwui2@gmail.com>
2022-03-08 14:40:41 -0500

    reverted codeback to previous state

--- setup-server/main.tf
+++ setup-server/main.tf
@@ -1,9 +1,8 @@
 # compile module aws-ec2
 module "instance" {
 
-  source  = "app.terraform.io/harshidevlearn/ec2/aws"
-  version = "~> 1.0.0"
-  create = true  # true = creates resource, false = does not create
+  source = "../modules/aws-ec2" # directory of the aws-ec2 module
+  create = true                 # true = creates resource, false = does not create
 
   ansible-playbook = var.playbook
 
